8409617ddeae3dc7b7bfd66266378df378fd53c9
Jon Jenkins <jon@mj12.su>
2017-10-22 22:18:28 -0500

    Continued work on Go library

--- go/bluepay/bluepay.go
+++ go/bluepay/bluepay.go
@@ -4,13 +4,21 @@ import (
 	"crypto/md5"
 	"encoding/hex"
 	"fmt"
+	"io"
+	"log"
 	"net/http"
 	"net/url"
 )
 
+//TransactionResult contains the results of the post
 type TransactionResult struct {
-	TransID string
-	Status  string
+	TransID            string
+	Status             string
+	HTTPSuccess        bool
+	HTTPResponseCode   int
+	HTTPResponseString string
+	Fields             map[string]string
+	ReponseBody        []byte
 }
 
 // Transaction contains information necessary to run transaction
@@ -20,10 +28,11 @@ type Transaction struct {
 	Mode      string
 	Fields    map[string]string
 	TPSDef    []string
+	Error     error
 }
 
 //APIURL is the API endpoint to connect to BluePay
-var APIURL = "http://localhost:8080"
+var APIURL = "https://secure.bluepay.com/interfaces/bp20post"
 
 // NewTransaction initializes and returns a Transaction struct
 func NewTransaction(mode, accountID, secretKey string) Transaction {
@@ -38,20 +47,44 @@ func NewTransaction(mode, accountID, secretKey string) Transaction {
 
 //SendTransaction sends the transaction to BluePay
 func (t Transaction) SendTransaction() TransactionResult {
+
 	t.calculateTps()
 	qstr := url.Values{}
 	for k, v := range t.Fields {
 		qstr.Add(k, v)
 	}
+	log.Println(qstr)
 	//sendStr := qstr.Encode()
 	r, err := http.PostForm(APIURL, qstr)
 	if err != nil {
 		fmt.Println(err)
-	} else {
-		fmt.Println(r)
 	}
 	var res TransactionResult
-	res.TransID = "999000000002"
+	if r.ContentLength < 1 {
+		res.ReponseBody = make([]byte, 8192)
+	} else {
+		res.ReponseBody = make([]byte, r.ContentLength)
+	}
+	read, err := r.Body.Read(res.ReponseBody)
+	if err != nil {
+		if err != io.EOF {
+			panic("Problem with request")
+		}
+	}
+	r.Body.Close()
+	res.ReponseBody = res.ReponseBody[:read]
+	res.HTTPResponseCode = r.StatusCode
+	res.HTTPResponseString = r.Status
+	res.Fields = make(map[string]string)
+	resValues, err := url.ParseQuery(string(res.ReponseBody))
+	fmt.Println(resValues)
+	if err != nil {
+		log.Println("Problem decoding response:", err)
+		return res
+	}
+	for k, v := range resValues {
+		res.Fields[k] = v[0]
+	}
 	return res
 }
 
